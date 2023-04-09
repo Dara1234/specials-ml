882235d648d54fe47be7bb4e5599c78a07f96857
Dara <tumebayevadara@gmail.com>
2023-04-09 13:06:18 +0600

    adding k means

--- python-scripts/clustering.py
+++ python-scripts/clustering.py
@@ -41,3 +41,114 @@ new_df = pd.concat([df, df_wrds], axis=1)
 
 # Dropping the Bios because it is no longer needed in place of vectorization
 new_df.drop('Bios', axis=1, inplace=True)
+
+
+
+# Importing the library
+from sklearn.decomposition import PCA
+
+# Instantiating PCA
+pca = PCA()
+
+# Fitting and Transforming the DF
+df_pca = pca.fit_transform(new_df)
+
+# Plotting to determine how many features should the dataset be reduced to
+plt.style.use("bmh")
+plt.figure(figsize=(14,4))
+plt.plot(range(1,new_df.shape[1]+1), pca.explained_variance_ratio_.cumsum())
+plt.show()
+
+# Finding the exact number of features that explain at least 95% of the variance in the dataset
+total_explained_variance = pca.explained_variance_ratio_.cumsum()
+n_over_95 = len(total_explained_variance[total_explained_variance>=.95])
+n_to_reach_95 = new_df.shape[1] - n_over_95
+
+# Printing out the number of features needed to retain 95% variance
+print(f"Number features: {n_to_reach_95}\nTotal Variance Explained: {total_explained_variance[n_to_reach_95]}")
+
+# Reducing the dataset to the number of features determined before
+pca = PCA(n_components=n_to_reach_95)
+
+# Fitting and transforming the dataset to the stated number of features and creating a new DF
+df_pca = pca.fit_transform(new_df)
+
+# Seeing the variance ratio that still remains after the dataset has been reduced
+print(pca.explained_variance_ratio_.cumsum()[-1])
+
+
+# Setting the amount of clusters to test out
+cluster_cnt = [i for i in range(2, 20, 1)]
+
+# Establishing empty lists to store the scores for the evaluation metrics
+s_scores = []
+
+db_scores = []
+
+# Looping through different iterations for the number of clusters
+for i in cluster_cnt:
+    
+    # Hierarchical Agglomerative Clustering with different number of clusters
+    hac = AgglomerativeClustering(n_clusters=i)
+    
+    hac.fit(df_pca)
+    
+    cluster_assignments = hac.labels_
+    
+    ## KMeans Clustering with different number of clusters
+    #k_means = KMeans(n_clusters=i)
+    
+    #k_means.fit(df_pca)
+    
+    #cluster_assignments = k_means.predict(df_pca)
+    
+    # Appending the scores to the empty lists    
+    s_scores.append(silhouette_score(df_pca, cluster_assignments))
+    
+    db_scores.append(davies_bouldin_score(df_pca, cluster_assignments))
+
+
+    def plot_evaluation(y, x=cluster_cnt):
+        """
+        Plots the scores of a set evaluation metric. Prints out the max and min values of the evaluation scores.
+        """
+        
+        # Creating a DataFrame for returning the max and min scores for each cluster
+        df = pd.DataFrame(columns=['Cluster Score'], index=[i for i in range(2, len(y)+2)])
+        df['Cluster Score'] = y
+        
+        print('Max Value:\nCluster #', df[df['Cluster Score']==df['Cluster Score'].max()])
+        print('\nMin Value:\nCluster #', df[df['Cluster Score']==df['Cluster Score'].min()])
+        
+        # Plotting out the scores based on cluster count
+        plt.figure(figsize=(16,6))
+        plt.style.use('ggplot')
+        plt.plot(x,y)
+        plt.xlabel('# of Clusters')
+        plt.ylabel('Score')
+        plt.show()
+        
+    # Running the function on the list of scores
+    plot_evaluation(s_scores)
+
+    plot_evaluation(db_scores)
+
+
+
+# Initializing HAC
+hac = AgglomerativeClustering(n_clusters=12)
+
+# Fitting
+hac.fit(df_pca)
+
+# Cluster assignments from HAC
+cluster_assignments = hac.labels_
+
+# Unscaling the categories then replacing the scaled values
+df = df[['Bios']].join(pd.DataFrame(scaler.inverse_transform(df.drop('Bios', axis=1)), columns=df.columns[1:], index=df.index))
+
+# Assigning the clusters to each profile
+df['Cluster #'] = cluster_assignments
+
+# Viewing the dating profiles with cluster assignments
+df
